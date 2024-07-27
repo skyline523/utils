6643be35ef72878d1927d6b9419791130af3706f
Leet <76251617+skyline523@users.noreply.github.com>
2024-07-27 18:54:21 +0800

    build: 删除unoconfig，使用unbuild打包

---
+++ build.config.ts
@@ -0,0 +1,16 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    'src/index',
+  ],
+  externals: ['unocss'],
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: true,
+    esbuild: {
+      minify: true,
+    },
+  },
+})
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from '../package/utils'
-export * from '../package/unocss'
